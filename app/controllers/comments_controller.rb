1a8a46de405638441d7164fc4be2118f6a456da1
sarah niemiec <sroseniemiec@gmail.com>
2017-04-27 11:23:01 -0400

    edit comment, deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,13 +17,48 @@ class CommentsController < ApplicationController
     )
     if @comment.save
       flash[:notice] = "Your comment has been made."
-      redirect_to "/"
+      redirect_to :back
     else
       flash[:alert] = "There was a problem with your comment, please try again."
       redirect_to new_post_comment
     end
   end
 
-  
+  def edit
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @user = @comment.user_id
+    if @user == session[:user_id]
+    else
+      flash[:alert] = "You do not have access to this page"
+      redirect_to "/"
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.content = params[:comment][:content]
+    if @comment.save
+      flash[:notice] = "Your comment has been updated"
+      redirect_to root_path
+    else
+      flash[:alert] = "There was a problem updating your post."
+      redirect_to edit_post_comment_path(@comment)
+    end
+
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "Your comment has been deleted."
+      redirect_to root_path
+    else
+      flash[:alert] = "Your comment could not be deleted."
+      redirect_to root_path
+    end
+
+  end
+
 
 end
