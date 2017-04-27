a25a5febae7053b939a99f9a7ea3af6f6b3d1081
sarah niemiec <sroseniemiec@gmail.com>
2017-04-27 10:18:42 -0400

    user can make a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,27 @@ class CommentsController < ApplicationController
   def index
 
   end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(
+    post_id: params[:post_id],
+    user_id: session[:user_id],
+    content: params[:comment][:content]
+    )
+    if @comment.save
+      flash[:notice] = "Your comment has been made."
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem with your comment, please try again."
+      redirect_to new_post_comment
+    end
+  end
+
   
+
 end
